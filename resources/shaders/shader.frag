3f1c787c2e6664f650b559c1bd640d49ff8d3e4f
Josh Carlson <jmcarlson0320@gmail.com>
2022-04-21 13:51:14 -0700

    initial commit for book_branch

---
+++ resources/shaders/shader.frag
@@ -0,0 +1,8 @@
+#version 330
+
+layout (location = 0) out vec4 out_fragmentColor;
+
+void main()
+{
+    out_fragmentColor = vec4(0.49f, 0.87f, 0.59f, 1.0f);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -15,196 +15,29 @@
 #define WIDTH 800
 #define HEIGHT 600
 
-#define TO_RADIANS(x) (x * 3.14159265f / 180.0f)
-
-// triangle parameters
-float x_offset = 0;
-float dx = 0.01;
-float angle = 0;
-float d_angle = 0.1;
-
-Mesh *mesh;
-Shader *shader;
-Texture *texture;
-
-// user input
-enum KEYBOARD_INPUTS {
-    FORWARD,
-    BACK,
-    LEFT,
-    RIGHT,
-    QUIT,
-    NUM_INPUTS
-};
-
-int KEY_MAP[NUM_INPUTS] = {
-    [FORWARD] = GLFW_KEY_W,
-    [BACK] = GLFW_KEY_S,
-    [LEFT] = GLFW_KEY_A,
-    [RIGHT] = GLFW_KEY_D,
-    [QUIT] = GLFW_KEY_Q
-};
-
-bool input[NUM_INPUTS] = {0};
-
-double mouse_x = 0.0f;
-double mouse_y = 0.0f;
-double mouse_dx = 0.0f;
-double mouse_dy = 0.0f;
-
-// camera
-glm::vec3 eye;
-glm::vec3 dir;
-
-void MessageCallback( GLenum source,
-                      GLenum type,
-                      GLuint id,
-                      GLenum severity,
-                      GLsizei length,
-                      const GLchar* message,
-                      const void* userParam )
+GLuint loadVertexData()
 {
-    fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
-           ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ), type, severity, message );
+    GLfloat vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+    GLuint triangleVBO[1];
+    glGenBuffers(1, triangleVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, triangleVBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, triangleVBO[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+    glBindVertexArray(0);
+
+    return triangleVBO[0];
 }
 
-// setup data and shaders for opengl draw calls
-void init()
-{
-    // enable debug output
-//    glEnable(GL_DEBUG_OUTPUT);
-//    glDebugMessageCallback(MessageCallback, 0);
-
-    // position data
-    GLfloat mesh_vertex_data[] = {
-    //   x      y      z     u     v
-        -1.0f, -1.0f,  0.0f, 0.0f, 0.0f,
-         0.0f, -1.0f,  1.0f, 0.5f, 0.0f,
-         1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
-         0.0f,  1.0f,  0.0f, 0.5f, 1.0f
-    };
-
-    // index data
-    unsigned int mesh_index_data[] = {
-        0, 3, 1,
-        1, 3, 2,
-        2, 3, 0,
-        0, 1, 2
-    };
-
-    mesh = new Mesh(mesh_vertex_data, sizeof(mesh_vertex_data[0]) * 20, mesh_index_data, 12);
-
-    // load shaders
-    shader = new Shader();
-    shader->fromFile("resources/shaders/uniforms.vert", "resources/shaders/fill_red.frag");
-
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glEnable(GL_DEPTH_TEST);
-
-    // init camera
-    eye = glm::vec3(0.0f, 0.0f, 1.0f);
-    dir = glm::vec3(0.0f, 0.0f, -1.0f);
-
-    // load texture
-    texture = new Texture();
-    texture->fromFile("resources/textures/bluerock_texture.jpg");
-    texture->use();
-}
-
-void handleInput(GLFWwindow *window)
-{
-    double prev_x = mouse_x;
-    double prev_y = mouse_y;
-
-    glfwPollEvents();
-
-    for (int i = 0; i < NUM_INPUTS; i++) {
-        input[i] = glfwGetKey(window, KEY_MAP[i]) == GLFW_PRESS;
-    }
-
-    glfwGetCursorPos(window, &mouse_x, &mouse_y);
-    mouse_dx = mouse_x - prev_x;
-    mouse_dy = prev_y - mouse_y; // up = pos
-}
-
-void update(float dt)
-{
-    // update camera
-    // get forward and right vectors from view direction
-    glm::vec3 forward = glm::vec3(dir.x, 0.0f, dir.z);
-    glm::vec3 right = glm::cross(dir, glm::vec3(0.0f, 1.0f, 0.0f));
-
-    // get delta pitch and yaw from mouse input
-    GLfloat delta_pitch = mouse_dy * 0.001;
-    GLfloat delta_yaw = mouse_dx * 0.001;
-
-    // rotation matrix for view pitch
-    glm::mat4 rotate_x = glm::rotate(glm::mat4(1.0f), delta_pitch, right);
-
-    // rotation matrix for view yaw
-    glm::mat4 rotate_y = glm::rotate(glm::mat4(1.0f), delta_yaw, -glm::vec3(0.0f, 1.0f, 0.0f));
-
-    // rotate the view direction by pitch first, then yaw
-    glm::vec4 dir_homog = rotate_y * rotate_x * glm::vec4(dir, 1.0f);
-    dir = glm::vec3(dir_homog);
-
-    // move camera position
-    if (input[FORWARD]) {
-        eye = eye + forward * 0.05f;
-    }
-    if (input[BACK]) {
-        eye = eye + forward * -0.05f;
-    }
-    if (input[LEFT]) {
-        eye = eye + right * -0.05f;
-    }
-    if (input[RIGHT]) {
-        eye = eye + right * 0.05f;
-    }
-}
-
-// call opengl draw functions
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // render the pyrimid
-    // activate shader
-    shader->use();
-
-    // update position
-    angle += d_angle;
-
-    // model matrix
-    glm::mat4 model(1.0f);
-    model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
-    model = glm::rotate(model, TO_RADIANS(angle), glm::vec3(0.0f, 1.0f, 0.0f));
-    glUniformMatrix4fv(shader->getModelLocation(), 1, GL_FALSE, glm::value_ptr(model));
-
-    // view matrix
-    glm::mat4 view(1.0f);
-    view = glm::lookAt(eye, eye + dir, glm::vec3(0.0f, 1.0f, 0.0f));
-    glUniformMatrix4fv(shader->getViewLocation(), 1, GL_FALSE, glm::value_ptr(view));
-
-    // projection matrix
-    glm::mat4 projection(1.0f);
-    GLfloat aspect_ratio = (GLfloat) WIDTH / (GLfloat) HEIGHT;
-    projection = glm::perspective(45.0f, aspect_ratio, 0.1f, 100.0f);
-    glUniformMatrix4fv(shader->getProjectionLocation(), 1, GL_FALSE, glm::value_ptr(projection));
-
-    // draw
-    mesh->render();
-
-    // deactivate shader
-    shader->clear();
-}
-
-// glfw code in here
-// creates a window and opengl context
-// calls init()
-// calls display() inside main loop
 int main(int argc, char *argv[])
 {
+    // initialize glfw
     if (!glfwInit()) {
         printf("could not init glfw\n");
         exit(1);
@@ -218,11 +51,8 @@ int main(int argc, char *argv[])
         exit(1);
     }
     glfwMakeContextCurrent(window);
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
 
-    // init glew
+    // initialize glew
     if(glewInit() != GLEW_OK) {
         printf("could not init glew\n");
         glfwDestroyWindow(window);
@@ -230,36 +60,44 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // setup triangle data and shaders
-    init();
+    // set viewport parameters
+    int nx, ny;
+    glfwGetFramebufferSize(window, &nx, &ny);
+    glViewport(0, 0, nx, ny);
+
+    // set background color
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    // load shaders
+    Shader shader;
+    shader.fromFile("./resources/shaders/shader.vert", "./resources/shaders/shader.frag");
+
+    // load triangle data
+    GLuint VAO = loadVertexData();
 
     // main loop
-    // float last = get_time();
-    float last = 0.0f;
     while(!glfwWindowShouldClose(window)) {
-        // float cur = get_time();
-        float cur = 0.0f;
-        float dt = cur - last;
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        handleInput(window);
-        update(dt);
-        display();
+        shader.use();
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        shader.clear();
 
         glfwSwapBuffers(window);
 
-        last = cur;
+        glfwPollEvents();
 
-        if (input[QUIT]) {
-            glfwSetWindowShouldClose(window, GL_TRUE);
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, 1);
         }
     }
 
     glfwDestroyWindow(window);
     glfwTerminate();
-
-    delete mesh;
-    delete shader;
-    delete texture;
 
     return 0;
 }
